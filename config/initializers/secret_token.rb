fc18a896c0547d4de46fddbfce37243c4cfd2e61
DeBraid <de.braid@gmail.com>
2013-10-24 14:26:24 -0400

    simple rails app to show off D3

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Viz::Application.config.secret_key_base = 'db656b1594a7e5c178d62fd2cac94f249f582c058060661b169290027d8e2fe320b4fb7da20b9bc157c09ede2ef60ee864f168e7b89f6c862f1969183b652299'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Viz::Application.config.session_store :cookie_store, key: '_Viz_session'
